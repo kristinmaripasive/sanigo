4794696fc83fd5605e41c5572867c2e1011ca4ce
Kristin Pasive <kristin.pasive@gmail.com>
2016-07-18 12:37:09 -0400

    created seeds and migrated

---
+++ db/migrate/20160718162154_create_comments.rb
@@ -0,0 +1,9 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :title
+      t.string :body
+      t.references :location, foreign_key: true
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@ locations = Location.create([
   {name: "General Assembly", address: "1133 15th St NW, Washington, DC 20005"},
 ])
 Comment.create([
-  {title: "First comment", body: "This is a great bathroom!" , location: location[0]},
-  {title: "Second comment", body: "This is a terrible place!" , location: location[1]},
+  {title: "First comment", body: "This is a great bathroom!" , location: locations[0]},
+  {title: "Second comment", body: "This is a terrible place!" , location: locations[1]},
 ])
