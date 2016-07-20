1ca7008448b8a339e584a601c0464ce608103c3a
Matthew Scott <matthewruelscott@gmail.com>
2016-07-20 09:57:43 -0400

    commit before new branch

--- db/migrate/20160718162030_create_locations.rb
+++ db/migrate/20160718162030_create_locations.rb
@@ -3,8 +3,9 @@ class CreateLocations < ActiveRecord::Migration
     create_table :locations do |t|
       t.string :name
       t.string :address
+      t.string :description
 
-    
+
     end
   end
 end
