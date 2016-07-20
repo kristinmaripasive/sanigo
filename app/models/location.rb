b0be7d7e8767ed0a21b9ff2eb9caa8dd21e90bfb
Kristin Pasive <kristin.pasive@gmail.com>
2016-07-20 12:10:32 -0400

    added lat and long columns

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,10 @@
 class Location < ApplicationRecord
   has_many :comments, dependent: :destroy
 
+
+
   def as_json(options={})
-    super(options).merge(Lat: 2)
+    super(options).merge(Lat: 2, Lng: 6)
   end
 
 end
